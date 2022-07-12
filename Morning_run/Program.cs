eaf14623da67be0afa5a2c0defdd3b04ee0e5a15
SilverXuz <silver-xuz@mail.ru>
2022-07-13 01:41:09 +0300

    Утренняя пробежка

---
+++ Morning_run/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Сколько КМ в первый день? Введите число от 0 до 1000: ");
+double x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Сколько КМ в день для поставленной цели? Введите число больше, чем первый день: ");
+int y = Convert.ToInt32(Console.ReadLine());
+while (y <= x)
+{
+    Console.WriteLine("Это число не подходит");
+    y = Convert.ToInt32(Console.ReadLine());
+}
+int day = 0;
+while (x < y)
+{
+    x = (x * 0.15 + x);
+    day++;
+}
+Console.Write(day + " дней");
