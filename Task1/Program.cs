b284bf2b02f2875e4da30a31a8c98bc2c840dcc5
SilverXuz <silver-xuz@mail.ru>
2022-07-12 23:45:45 +0300

    Программа, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите трехначное число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+while (x <= 99 || x > 999)
+{
+    Console.WriteLine("Это число не подходит");
+    x = Convert.ToInt32(Console.ReadLine());
+}
+Console.Write("Вторая цифра этого числа: ");
+Console.Write(Convert.ToString(x)[1]);
+
+
+
+// Альтернатива с рандомным числом
+// int x = new Random().Next(100,1000);
+// Console.WriteLine(x);
+// Console.Write(Convert.ToString(x)[1]);
