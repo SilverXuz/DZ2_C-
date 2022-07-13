949568adefc49c7c55297ce0fadbf991e866b427
SilverXuz <silver-xuz@mail.ru>
2022-07-13 23:25:25 +0300

    Программа,которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,11 +1,14 @@
 ﻿Console.Write("Введите число: ");
 int x = Convert.ToInt32(Console.ReadLine());
-if (x < 0)
-    x = x * (-1);
-else
-    x = x * 1;
+x = Math.Abs(x);
+// Мой первый вариант был такой
+// if (x < 0)
+//    x = x * (-1);
+// else
+//    x = x * 1;
     
 if (x >= 0 && x < 100)
     Console.Write("Третьего числа нет!");
 else
     Console.Write(Convert.ToString(x)[2]);
+    
